4070f3461d5c4272ff6aff9870dc621a5f3d1065
tiwariaman617 <65229934+tiwariaman617@users.noreply.github.com>
2020-05-21 18:46:19 +0530

    ProgAss2
    Done By Aman

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+   inv<-x$getInverse()
+   if(!is.null(inv)){
+      message("getting cache data")
+      return(inv)
+   }
+  data<-x$get()
+  inv<-solve(data)
+  x$setInverse(inv)
+  inv
 }
